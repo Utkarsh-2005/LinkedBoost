54795511f8c7e41cef6879588f5686e1967dc8f1
Utkarsh <utkarshjha.4009@gmail.com>
2025-03-12 22:49:30 +0530

    Final try

--- app/api/credits/getcredits/route.ts
+++ app/api/credits/getcredits/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
-import { authOption } from "@/app/api/auth/[...nextauth]/route";
+import { authOption } from '@/lib/authOption'
 import {prisma} from "@/lib/prisma";
 
 export async function GET(request: Request) {
